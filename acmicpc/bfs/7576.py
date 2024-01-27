0ea9527f3912cc17d96db90be821f9259f590381
PEJEUNJI <pej870922@gmail.com>
2024-01-28 03:35:35 +0900

    bfs-최단 소요 시간 구하기

---
+++ acmicpc/bfs/7576.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+col, row = map(int, sys.stdin.readline().split())
+
+#입력값 저장
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(row)]
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+queue = deque()
+#이미 익은 토마토는 queue 에 넣고 시작한다. 안그러면 중복 발생
+for r in range(row) :
+    for c in range (col) :
+       if graph[r][c] == 1 :
+           queue.append((r,c))
+
+
+total_days = 0
+def bfs(graph) :
+    while queue:
+        current_x, current_y = queue.popleft()
+        for index in range(4) :
+           after_x =  current_x + dx[index]
+           after_y =  current_y + dy[index]
+           # queue에 이미 익은 토마토만 있으므로 안익은 토마토만 찾아서 변경 시켜 준다
+           if 0<=after_x<row and 0<=after_y<col and graph[after_x][after_y] == 0 :
+                graph[after_x][after_y] = graph[current_x][current_y] + 1
+                queue.append((after_x,after_y))
+   
+bfs(graph)
+max_value = 0
+for r in range(row) :
+    for c in range(col) :
+        #모든 탐색을 다 했는데도 안익은 토마토가 있다면 모두 익지 못하는 상황
+        if graph[r][c] == 0 : 
+            print(-1)
+            sys.exit()
+        #최대값 찾기
+        if(max_value < graph[r][c]) : 
+            max_value = graph[r][c]
+
+#처음 익은 토마토의 1은 빼줘야 하므로 
+print(max_value-1)
